814897d6e2dd38c436502987b314dc530df1ad87
Haseeb Munir <hamunir4542@hotmail.com>
2017-02-25 13:37:05 -0500

    SignUp and Profile layouts implemented

---
+++ UserSystem/app/src/main/java/com/example/haseeb/usersystem/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.example.haseeb.usersystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sign_up, menu);
+        return true;
+    }
+}
